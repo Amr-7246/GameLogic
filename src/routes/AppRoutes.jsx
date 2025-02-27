36fa43299c4581e267a9ca370b8581795ea61723
Amr-7246 <aehab7246@gmail.com>
2025-02-27 17:53:11 +0200

    Done

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import MainPage from '../layouts/MainPage';
-import Profile from '../layouts/Profile';
-import Login from '../layouts/Login';
 import NotFound from '../layouts/NotFound';
-import Home from '../pages/Home';
 import MemoryBlocks from '../pages/MemoryBlocks';
-import GuessWord from '../pages/GuessWord';
 import HangMan from '../pages/HangMan';
+// import Home from '../pages/Home';
+// import GuessWord from '../pages/GuessWord';
 
 const AppRoutes = () => {
   const location = useLocation(); 
@@ -18,13 +16,12 @@ const AppRoutes = () => {
       <Routes location={location} key={location.pathname} >
         <Route path="/" element={<MainPage />}>
           <Route index element={<Navigate to="HangMan" replace />} />
-          <Route path="home" element={<Home />} />
+          {/* <Route path="home" element={<Home />} /> */}
           <Route path="memoryBlocks" element={<MemoryBlocks />} />
-          <Route path="guessWord" element={<GuessWord />} />
+          {/* <Route path="guessWord" element={<GuessWord />} /> */}
           <Route path="hangMan" element={<HangMan />} />
         </Route>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </AnimatePresence>
